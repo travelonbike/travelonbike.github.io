a09ad3fc09a6268ba36f9139649bf4dd7c0d03a1
Lepoldo Caballero <git@pitakill.net>
2015-07-10 22:26:19 -0500

    Putting this shit together.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
+//YouTube video shit.
+var loader = $('.loader'),
+    cover = $('.cover');
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/player_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('player', {
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange},
+    height: '100%',
+    playerVars: {
+      'autoplay': 1,
+      'controls': 0,
+      'loop': 1,
+      'rel': 0,
+      'showinfo': 0},
+    videoId: 'ZvrjQOHJIAA',
+    width: '100%'
+ });
+};
+function onPlayerReady(event) {
+  event.target.mute();
+  loader.css('opacity',0);
+  window.setTimeout(function(){
+    loader.remove();
+  }, 500);
+};
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING) {
+    $('.cover').css('opacity',0);
+  }
+};
+
+//jQuery after everything loads.
 $(function(){
 'use strict';
 
@@ -21,7 +59,39 @@ function resizeVideo() {
   console.log('Resizing!');
 };
 
+function enter() {
+  alert("Click");
+};
+
 resizeVideo();
 $(window).resize(resizeVideo);
 
+$('main > section').click(enter);
+
+//SVG shit for use CSS
+/*
+ * Replace all SVG images with inline SVG
+ */
+jQuery('img.svg').each(function(){
+  var $img = jQuery(this);
+  var imgID = $img.attr('id');
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  jQuery.get(imgURL, function(data) {
+    // Get the SVG tag, ignore the rest
+    var $svg = jQuery(data).find('svg');
+    // Add replaced image's ID to the new SVG
+    if(typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+    }
+    // Add replaced image's classes to the new SVG
+    if(typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass+' replaced-svg');
+    }
+    // Remove any invalid XML tags as per http://validator.w3.org
+    $svg = $svg.removeAttr('xmlns:a');
+    // Replace image with new SVG
+    $img.replaceWith($svg);
+  }, 'xml');
+});
 });
